c81b61c56f3e703acb0beb89db82703e616d2c96
juniornelson123 <junior123nelson@gmail.com>
2017-05-24 22:49:07 -0300

    adicionando billingCycle

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -1,10 +1,32 @@
 import React, {Component} from 'react'
 
+import Content from '../common/template/content'
+import ContentHeader from '../common/template/contentHeader'
+import Tabs from '../common/tab/tabs'
+import TabsHeader from '../common/tab/tabsHeader'
+import TabsContent from '../common/tab/tabsContent'
+import TabHeader from '../common/tab/tabHeader'
+
 class BillingCycle extends Component{
 	
 	render(){
 		return(
-			<h1>BillingCycle</h1>
+			<div>
+				<ContentHeader title="Ciclo de pagamento" small="cadastro"/>
+				<Content>		
+					<Tabs>
+						<TabsHeader>
+							
+						</TabsHeader>
+						<TabsContent>
+							<TabHeader label="Listar" icon="bars" target="tabList" />
+							<TabHeader label="Incluir" icon="plus" target="tabCreate" />
+							<TabHeader label="Alterar" icon="pencil" target="tabUpdate" />
+							<TabHeader label="Deletar" icon="trash-o" target="tabDelete" />
+						</TabsContent>
+					</Tabs>		
+				</Content>	
+			</div>
 			
 		)
 	}
