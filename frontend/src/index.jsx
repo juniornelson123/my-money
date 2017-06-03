798e8ba40a04a859f76a50b69509791d7e984335
juniornelson123 <junior123nelson@gmail.com>
2017-06-03 00:19:15 -0300

    adicionando msg e cadastro

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import promise from 'redux-promise'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
 
 import App from './main/app'
 import reducers from './main/reducers'
 
-const store = applyMiddleware(promise)(createStore)(reducers)
+const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers)
 ReactDOM.render(
 	<Provider store={store}>
 		<App />
